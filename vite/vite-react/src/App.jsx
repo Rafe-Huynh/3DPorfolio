1ecb33c1858bafd710296c7bb1d03b638746127f
Thai Huynh <rafehuynh0510@gmail.com>
2024-09-11 19:57:35 -0500

    basic setup

---
+++ vite/vite-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <h1 className='text-3xl text-white underline'>
+        Hello
+      </h1>
+    </main>
+  )
+}
+export default App
