e6e56dd32505fb2d12892fd315348893bce56e6d
Thai Huynh <rafehuynh0510@gmail.com>
2024-09-12 20:17:25 -0500

    fixed some error related to hacker room

--- src/components/CanvasLoader.jsx
+++ src/components/CanvasLoader.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Html, useProgress } from '@react-three/drei'
 const CanvasLoader = () => {
-    const {progess} = useProgress()
+    const {progress} = useProgress()
   return (
-    <Html as="div" center style={{display: flex, justifyContent: center, alignItems: center, flexDirection: 'column'}}>
+    <Html as="div" center style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
         <span className='canvas-loader'/>
         <p style={{fontSize: 14, color: "#F1F1F1", fontWeight: 800, marginTop: 40}}>
-            {
-                progess !== 0 ? `${progess.tofixed(2)}%` : "Loading..." 
-            }
+        {progress !== 0 ? `${progress.toFixed(2)}%` : 'Loading...'}
         </p>
         </Html>
   )
--- src/sections/MainPage.jsx
+++ src/sections/MainPage.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react'
 import {Canvas} from "@react-three/fiber"
-import { PerspectiveCamera } from 'three'
+import { PerspectiveCamera } from '@react-three/drei';
 import HackerRoom from '../components/HackerRoom'
 import CanvasLoader from '../components/CanvasLoader'
 const MainPage = () => {
@@ -15,7 +15,7 @@ const MainPage = () => {
         </p>
         </div>
         <div className='w-full h-full absolute inset-0'>
-          {/* <Canvas className='w-full h-full'>
+          <Canvas className='w-full h-full'>
             <Suspense fallback={<CanvasLoader/>}>
             <PerspectiveCamera makeDefault position={[0,0,30]} />
             <HackerRoom scale={0.1} position={[0,0,0]} rotation={[0, -Math.PI/ 2, 0]}/>
@@ -23,7 +23,7 @@ const MainPage = () => {
             <directionalLight position={[10,10,10]} intensity={0.5}/>
             </Suspense>
           
-          </Canvas> */}
+          </Canvas>
         </div>
     </section>
   )
