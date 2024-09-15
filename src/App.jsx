b552a30be891368a8da98d9c9e2362e74f578d28
Thai Huynh <rafehuynh0510@gmail.com>
2024-09-14 21:02:57 -0500

    about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import MainPage from './sections/MainPage'
-
+import About from './sections/About'
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar />
       <MainPage />
+      <About />
     </main>
   )
 }
