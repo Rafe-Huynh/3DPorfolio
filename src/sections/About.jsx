6b2cac2bd34508d8ad951f37d8e2e2bc07c4d605
Thai Huynh <rafehuynh0510@gmail.com>
2024-09-18 14:44:50 -0500

    updated about me section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Globe from 'react-globe.gl';
-
+import { MdAttachEmail } from "react-icons/md";
 import Button from '../components/Button.jsx';
 
 const About = () => {
@@ -23,7 +23,7 @@ const About = () => {
             <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
             <div>
-              <p className="grid-headtext">Hi, I’m Adrian Hajdin</p>
+              <p className="grid-headtext">I'm Rafe Huynh</p>
               <p className="grid-subtext">
               I'm a software engineer.
         I understand the importance of staying ahead in tech. <br/>
@@ -39,10 +39,10 @@ const About = () => {
             <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
 
             <div>
-              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-headtext">Tech</p>
               <p className="grid-subtext">
-                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                applications
+                I specialize in Python and Javascript, React frameworks, and tools that allow me to build robust and scalable
+                applications.
               </p>
             </div>
           </div>
@@ -64,40 +64,50 @@ const About = () => {
               />
             </div>
             <div>
-              <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+              <p className="grid-headtext">I’m flexible with locations</p>
+              <p className="grid-subtext">I can relocate anywhere in the US</p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
             </div>
           </div>
         </div>
 
         <div className="xl:col-span-2 xl:row-span-3">
-          <div className="grid-container">
-            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+          <div className='grid-container'>
+
+          <div className="grid grid-cols-2 gap-0">
+            <div className='grid-container2'>
+            <img src="assets/bmcc.jpg" alt="bmcc" className="w-full sm:h-[266px] h-fit object-contain" />
 
             <div>
-              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-headtext">Associate of Art in Computer Science</p>
               <p className="grid-subtext">
-                I love solving problems and building things through code. Programming isn&apos;t just my
-                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+                Graduated from Borough of Manhattan Community College of The City University of New York
               </p>
             </div>
+            </div>
+            <div className='grid-container2'>
+            <img src="assets/hunter college.jpg" alt="hunter" className="w-full sm:h-[266px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Bachelor of Art in Computer Science</p>
+              <p className="grid-subtext">
+                Graduated from Hunter College of The City University of New York 
+              </p>
+            </div>
+            </div>
+          </div>
           </div>
         </div>
 
         <div className="xl:col-span-1 xl:row-span-2">
           <div className="grid-container">
-            <img
-              src="assets/grid4.png"
-              alt="grid-4"
-              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
-            />
-
+            <div className='flex flex-col items-center justify-center text-white-600'>
+          <MdAttachEmail size={50}/>
+          </div>
             <div className="space-y-2">
-              <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">adrian@jsmastery.pro</p>
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">rafehuynh0510@gmail.com</p>
               </div>
             </div>
           </div>
